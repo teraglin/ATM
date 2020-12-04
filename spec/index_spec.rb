676e26719ee9d60f8fa5d880ddc46a42bee96cf2
teraglin <apkirvan@hotmail.com>
2020-12-04 11:42:56 +1100

    initialised bundler. added rspec

---
+++ spec/index_spec.rb
@@ -0,0 +1,11 @@
+require '../index'
+
+describe 'index.rb' do
+    it 'says hello' do
+        expect(hello()).to eq("Hello World")
+    end
+
+    xit 'does something else' do
+        
+    end
+end
